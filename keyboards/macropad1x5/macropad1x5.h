8803aeb04a9b5f513e2628114661f728476da6fd
Leon S <leonstubbig@web.de>
2021-07-26 20:27:16 +0200

    Update email

--- keyboards/macropad1x5/macropad1x5.h
+++ keyboards/macropad1x5/macropad1x5.h
@@ -1,4 +1,4 @@
-/* Copyright 2020 Leon Stubbig <leonstubbig@web.de>
+/* Copyright 2020 Leon Stubbig <leonskeyboards@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
