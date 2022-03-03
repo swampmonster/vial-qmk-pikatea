5191775fb8f947754a1828c490740592bcce0374
Jack Kester <66742583+JackPikatea@users.noreply.github.com>
2022-03-03 13:02:15 -0600

    add tap dance to GB3 and FK1

--- keyboards/pikatea/pikatea_macropad_fk1/rules.mk
+++ keyboards/pikatea/pikatea_macropad_fk1/rules.mk
@@ -20,6 +20,6 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
-TAP_DANCE_ENABLE = no
+TAP_DANCE_ENABLE = yes
 ENCODER_ENABLE = yes
 
